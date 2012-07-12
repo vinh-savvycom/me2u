1b2f6e66e124cd888826d5ff031f6ddf0744cc5d
lequangvinh1989@gmail.com <lequangvinh1989@gmail.com@4b1a19cb-69ac-1c42-03b6-ee2cffed55fb>
2012-07-12 02:51:25 +0000

    add product detail

---
+++ trunk/Me2U/Me2U/DataAccess/ProductDetail/ProductDetail.m
@@ -0,0 +1,28 @@
+//
+//  ProductDetail.m
+//  Me2U
+//
+//  Created by Le Quang Vinh on 7/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ProductDetail.h"
+
+@implementation ProductDetail
+
+@synthesize shopProduct;
+@synthesize priceProduct;
+@synthesize titleProduct;
+@synthesize linkToImgProduct;
+@synthesize descriptionProduct;
+@synthesize manufacturerProduct;
+
+- (id)init {
+    self = [super init];
+	if (self) {
+
+	}
+	return self;
+}
+
+@end
